ed3e86d019bc4b9cf7591cc8018754bae2a78a53
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2022-01-03 23:51:33 +0200

    Implemented ChangeScore and IncreaseLevel. Left only getPercent and averageHighest not including the bonus.

--- PlayerManager.cpp
+++ PlayerManager.cpp
@@ -86,5 +86,49 @@ namespace wet2_dast{
         }
     }
 
+    StatusType PlayerManager::increasePlayerIDLevel(int PlayerID, int LevelIncrease)
+    {
+        if(PlayerID <= 0 || LevelIncrease <= 0)
+            return INVALID_INPUT;
+        try
+        {
+            Group* group = groups->find(0);
+            Player* player = group->findPlayer(PlayerID);
+            Group* players_group = groups->find(player->getGroup());
+            players_group->increaseLevelToPlayer(PlayerID, LevelIncrease);
+            group->increaseLevelToPlayer(PlayerID, LevelIncrease);
+        }
+        catch (HashTable<Player>::exceptions& e)
+        {
+            return FAILURE;
+        }
+        catch (std::exception& e)
+        {
+            return ALLOCATION_ERROR;
+        }
+    }
+
+    StatusType PlayerManager::changePlayerIDScore(int PlayerID, int NewScore)
+    {
+        if(PlayerID <= 0 || NewScore <= 0 || NewScore > scale)
+            return INVALID_INPUT;
+        try
+        {
+            Group* group = groups->find(0);
+            Player* player = group->findPlayer(PlayerID);
+            Group* players_group = groups->find(player->getGroup());
+            players_group->increaseScoreToPlayer(PlayerID, NewScore);
+            group->increaseScoreToPlayer(PlayerID, NewScore);
+        }
+        catch (HashTable<Player>::exceptions& e)
+        {
+            return FAILURE;
+        }
+        catch (std::exception& e)
+        {
+            return ALLOCATION_ERROR;
+        }
+    }
+
 
 } //namespace wet2_dast
