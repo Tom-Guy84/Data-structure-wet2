2d84e53c11a8d577bd0f56d02f2af3a867657087
Transfer Project <zivdamir@campus.technion.ac.il>
2022-01-04 16:35:59 +0200

    added DS==NULL CONDITION

--- library2.cpp
+++ library2.cpp
@@ -17,49 +17,74 @@ void *Init(int k, int scale) {
 }
 
 StatusType MergeGroups(void *DS, int GroupID1, int GroupID2) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->mergeGroups(GroupID1, GroupID2);
 
 }
 
 StatusType AddPlayer(void *DS, int PlayerID, int GroupID, int score) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->addPlayer(PlayerID, GroupID, score);
 }
 
 StatusType RemovePlayer(void *DS, int PlayerID) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->removePlayer(PlayerID);
 }
 
 StatusType IncreasePlayerIDLevel(void *DS, int PlayerID, int LevelIncrease) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->increasePlayerIDLevel(PlayerID, LevelIncrease);
 }
 
 StatusType ChangePlayerIDScore(void *DS, int PlayerID, int NewScore) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->changePlayerIDScore(PlayerID, NewScore);
 }
 
 StatusType GetPercentOfPlayersWithScoreInBounds(void *DS, int GroupID, int score, int lowerLevel, int higherLevel,
                                                 double *players) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->getPercentOfPlayersWithScoreInBounds(GroupID, score, lowerLevel, higherLevel, players);
 }
 
 StatusType AverageHighestPlayerLevelByGroup(void *DS, int GroupID, int m, double *level) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->averageHighestPlayerLevelByGroup(GroupID, m, level);
 }
 
 StatusType
 GetPlayersBound(void *DS, int GroupID, int score, int m, int *LowerBoundPlayers, int *HigherBoundPlayers) {
+    if (DS == NULL) {
+        return INVALID_INPUT;
+    }
     auto *DS_OK = (PlayerManager *) DS;
     return DS_OK->getPlayersBound(GroupID, score, m, LowerBoundPlayers, HigherBoundPlayers);
 }
 
 void Quit(void **DS) {
+
     if (!DS) {
         return;
     }
